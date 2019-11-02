e53080cfb0cf21b4d4206e29cb7269c68426c053
harris222 <harris222@users.noreply.github.com>
2019-11-02 14:41:59 -0400

    Second commit

--- app.js
+++ app.js
@@ -39,25 +39,80 @@ app.listen(port, () => {
 
 
 
-// Database Name
-const dbName = 'myproject';
-const client = new MongoClient(url, {useNewUrlParser: true});
+var MongoClient = require('mongodb').MongoClient
+  , assert = require('assert');
 
-// Use connect method to connect to the server
-client.connect(function(err) {
+var uri = "mongodb://newUser:1@cluster0-shard-00-00-qwtwk.mongodb.net:27017,cluster0-shard-00-01-qwtwk.mongodb.net:27017,cluster0-shard-00-02-qwtwk.mongodb.net:27017/test?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin&retryWrites=true&w=majority";
+MongoClient.connect(uri, function(err, client) {
   assert.equal(null, err);
   console.log("Connected successfully to server");
 
-  const db = client.db(dbName);
+  var collection = client.db("condo19").collection("condo19");
+  var myQuery = {type : "chara"};
 
-  insertDocuments(db, function() {
-    client.close();
+  //find everything
+  collection.find(myQuery).toArray(function(err, docs){
+    if(err) throw err;
+    console.log(docs);
+    console.log("Found " + docs.length + " documents.");
+
+    for(var i in docs){
+      console.log("My name is " + docs[i].name);
+      for(var j in docs[i].friendnames){
+        var frname = docs[i].friendnames[j];
+        collection.findOne({"name": frname}, function(err, friend){
+          if(err) throw err;
+          console.log("My friend " + friend.name + " is " + friend.activity + " " + friend.specific);
+        });
+      }
+    }
+  client.close();
   });
 });
 
-const insertDocuments = function(db, callback) {
+
+// // Connection URL
+// var url = 'mongodb://localhost:27017/myproject';
+//
+// // Use connect method to connect to the server
+// MongoClient.connect(url, function(err, db) {
+//   assert.equal(null, err);
+//   console.log("Connected successfully to server");
+//
+//   var collection = db.collection("documents");
+//   var myQuery = {type : "chara"};
+//
+//   //delete everything
+//   // collection.deleteMany(myQuery, function(err, obj){
+//   //   if (err) throw err;
+//   //   console.log(obj.result.n + " document(s) deleted");
+//   //   db.close();
+//   // });
+//
+//   //find everything
+//   collection.find(myQuery).toArray(function(err, docs){
+//     if(err) throw err;
+//     console.log(docs);
+//     console.log("Found " + docs.length + " documents.");
+//     for(var i in docs){
+//       console.log("My name is " + docs[i].name);
+//       for(var j in docs[i].friendnames){
+//         var frname = docs[i].friendnames[j];
+//         var frq = {name: frname};
+//         collection.findOne(frq, function(err, friend){
+//           if(err) throw err;
+//           console.log("My friend " + friend.name + " is " + friend.activity + " " + friend.specific);
+//         });
+//       }
+//     }
+//     db.close();
+//   });
+// });
+
+//add documents to server
+var insertDocuments = function(db, callback) {
   // Get the documents collection
-  const collection = db.collection('documents');
+  var collection = db.collection('documents');
   // Insert some documents
   collection.insertMany([
     {a : 1}, {a : 2}, {a : 3}
@@ -66,6 +121,37 @@ const insertDocuments = function(db, callback) {
     assert.equal(3, result.result.n);
     assert.equal(3, result.ops.length);
     console.log("Inserted 3 documents into the collection");
+    callback(result);
+  });
+}
+
+//get all documents from server
+var findDocuments = function(db, callback) {
+  // Get the documents collection
+  var collection = db.collection('documents');
+  // Find some documents
+  collection.find({'a' : 2}).toArray(function(err, docs) {
+    //assert.equal(err, null);
+    console.log(err);
+    console.log("Found the following records");
+    console.log(docs);
+    for(var each in docs){
+      console.log(docs[each].a);
+    }
+    callback(docs);
+  });
+}
+
+//update a document
+var updateDocument = function(db, callback) {
+  // Get the documents collection
+  var collection = db.collection('documents');
+  // Update document where a is 2, set b equal to 1
+  collection.updateOne({ a : 2 }
+    , { $set: { b : 1 } }, function(err, result) {
+    assert.equal(err, null);
+    assert.equal(1, result.result.n);
+    console.log("Updated the document with the field a equal to 2");
     callback(result);
   });
 }
